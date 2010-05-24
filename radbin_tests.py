ecb18f851486cc632da5881becd4e7c9212d7e03
Teemu Ikonen <tpikonen@gmail.com>
2010-05-24 15:15:32 +0200

    radbin_tests.py: Improve tests.

--- radbin_tests.py
+++ radbin_tests.py
@@ -1,8 +1,8 @@
 import radbin as r
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-def corners_test():
+def coordinate_test():
     m = np.zeros((9,9))
     m[0,4] = 1
     m[8,4]= 1
@@ -12,6 +12,23 @@ def corners_test():
 #    plt.plot(I)
 #    plt.show()
 #    plt.waitforbuttonpress()
-    print(I[4])
+#    print(I[4])
     assert(I[4] == 4.0)
+    m = np.zeros((10,5))
+    m[0,3] = 1
+    (I,n) = r.radbin(m, c_x=3.5, c_y=5.5, radrange=np.arange(0,10), norm=0)
+#    plt.plot(I)
+#    plt.show()
+    assert(I[5] == 1.0)
+    I[5] = 0.0
+    assert(not I.all())
+    (I,n) = r.radbin(m, c_x=3.5, c_y=0.5, radrange=np.arange(0,10), norm=0)
+    assert(I[0] == 1.0)
+    I[0] = 0.0
+    assert(not I.all())
+
+def symmetric_matrix():
+    xx = np.linspace(-1, 1, num=100)
+    ons = np.ones_like(xx)
+    rr = sqrt((np.outer(ons, xx**2)+np.outer(ons, xx**2).T))
 
