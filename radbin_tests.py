bc0ab56a0c1868bf66dd73e8a4b95d9444f5d20e
Teemu Ikonen <tpikonen@gmail.com>
2011-02-09 16:20:41 +0100

    Add a basic test for binstats().

--- radbin_tests.py
+++ radbin_tests.py
@@ -32,3 +32,28 @@ def symmetric_matrix():
     ons = np.ones_like(xx)
     rr = sqrt((np.outer(ons, xx**2)+np.outer(ons, xx**2).T))
 
+def binstats_test():
+    lin = np.arange(10)
+    inds = np.zeros((4), dtype=np.object)
+    inds[0] = np.ones((len(lin)), dtype=np.int32)
+    inds[1] = np.array([])
+    inds[2] = np.array(1)
+    inds[3] = lin
+    res = r.binstats(inds, lin, (1, 1, 1, 1))
+    assert(res[0][0] == 1.0)
+    assert(np.isnan(res[0][1]))
+    assert(res[0][2] == 1.0)
+    assert(res[0][3] == np.mean(lin))
+    assert(res[1][0] == 1.0)
+    assert(np.isnan(res[1][1]))
+    assert(res[1][2] == 1.0)
+    assert(res[1][3] == np.mean(lin))
+    assert(res[2][0] == 0.0)
+    assert(np.isnan(res[2][1]))
+    assert(res[2][2] == 0.0)
+    assert(res[2][3] == np.std(lin)/np.sqrt(len(lin)))
+    assert(res[3][0] == np.sqrt(len(lin))/len(lin))
+    assert(np.isnan(res[3][1]))
+    assert(res[3][2] == 1.0)
+    assert(res[3][3] == np.sqrt(np.sum(lin))/len(lin))
+#    return res
